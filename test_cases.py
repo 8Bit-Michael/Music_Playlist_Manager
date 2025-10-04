15f7644bfb4708ef96d8c2e629df7d1ccc17df0c
8-Bit Michael <mowz2012@gmail.com>
2025-10-03 22:13:08 -0400

    Add files via upload

---
+++ test_cases.py
@@ -0,0 +1,9 @@
+from playlist import SongNode
+from playlist import Playlist
+
+def test_function():
+    song = SongNode()
+    result = song.playlist_display()
+    print(result)
+
+test_function()
