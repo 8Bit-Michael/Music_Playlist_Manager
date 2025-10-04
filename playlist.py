96ffd70afd10d95e4acbe8cc7327bad9eaff6f2b
8-Bit Michael <mowz2012@gmail.com>
2025-10-03 22:12:40 -0400

    Add files via upload

---
+++ playlist.py
@@ -0,0 +1,22 @@
+class SongNode: # Represents one song.
+    def playlist_display(): 
+        return "[Way Out There] - [Lord Huron]"
+
+class Playlist: # Manages the list and navigation.
+    def add_song(title, artist):
+        pass
+
+    def remove_song(title):
+        pass
+
+    def play_next():
+        pass
+
+    def play_previous():
+        pass
+ 
+    def display_playlist():
+        pass
+
+    def show_current_song():
+        pass
