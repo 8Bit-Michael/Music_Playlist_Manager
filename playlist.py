741a4d9068fd0292924798397884946dbfe62c67
8-Bit Michael <mowz2012@gmail.com>
2025-10-05 22:39:26 -0400

    Updated: playlist.py
    I created the attributes in the SongNode and Playlist class, along with a playlist_display() test function to use, in addition with the test_cases.py module, to be able to create an object of the class in a separate module for testing purposes.

--- playlist.py
+++ playlist.py
@@ -1,22 +1,37 @@
-class SongNode: # Represents one song.
-    def playlist_display(): 
-        return "[Way Out There] - [Lord Huron]"
+class SongNode:
+    # Represents one song.
+    def __init__(self, title, artist, next=None, prev=None):
+        self.title = title
+        self.artist = artist
+        self.next = next
+        self.prev = prev
 
-class Playlist: # Manages the list and navigation.
-    def add_song(title, artist):
+    def playlist_display(self): 
+        return f"[{self.title}] - [{self.artist}]"
+
+
+class Playlist:
+    # Manages the list and navigation.
+    def __init__(self):
+        self.current = None
+
+    def is_empty(self):
         pass
 
-    def remove_song(title):
+    def add_song(self, title, artist):
         pass
 
-    def play_next():
+    def remove_song(self, title):
         pass
 
-    def play_previous():
-        pass
- 
-    def display_playlist():
+    def play_next(self):
         pass
 
-    def show_current_song():
+    def play_previous(self):
+        pass
+
+    def display_playlist(self):
+        pass
+
+    def show_current_song(self):
         pass
