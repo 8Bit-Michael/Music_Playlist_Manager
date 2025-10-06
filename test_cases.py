dc0c41892a4738381b8b47a744e1d7bab7bf56cc
8-Bit Michael <mowz2012@gmail.com>
2025-10-06 16:50:44 -0700

    Updated: test_cases.py
    I added a function to add some additional nodes to the playlist in a more efficient way using the add_song() function in the playlist.py module.

--- test_cases.py
+++ test_cases.py
@@ -1,7 +1,7 @@
 from playlist import SongNode, Playlist
 
 def test_function():
-    song = SongNode("Way Out There", "Lord Huron")
+    song = SongNode("The Night We Met", "Lord Huron")
     result = song.playlist_display()
     print(result)
 
@@ -14,5 +14,14 @@ def test_node():
     )
     print(node.playlist_display())
 
+def test_add_song():
+    playlist = Playlist()
+    playlist.add_song("All The Right Moves", "OneRepublic")
+    playlist.add_song("Standing In The Middle Of The Field", "Haiku From Zero")
+    playlist.add_song("Miol Mor Mara", "Outsider")
+    playlist.add_song("Way Out There", "Lord Huron")
+    playlist.display_playlist()
+
 test_function()
 test_node()
+test_add_song()
