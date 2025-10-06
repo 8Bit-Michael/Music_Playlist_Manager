fe1e9b37cd2a37c6fe7b167f2c4d64b34150dad9
8-Bit Michael <mowz2012@gmail.com>
2025-10-05 22:41:52 -0400

    Updated: test_cases.py
    I added a test_node() function in order to learn and play around with how a node object should be created.

--- test_cases.py
+++ test_cases.py
@@ -1,9 +1,18 @@
-from playlist import SongNode
-from playlist import Playlist
+from playlist import SongNode, Playlist
 
 def test_function():
-    song = SongNode()
+    song = SongNode("Way Out There", "Lord Huron")
     result = song.playlist_display()
     print(result)
 
+def test_node():
+    node = SongNode(
+        title="Do You Remember The Times",
+        artist="ISLAND",
+        next=None,
+        prev=None
+    )
+    print(node.playlist_display())
+
 test_function()
+test_node()
