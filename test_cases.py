352a6b24c4d751c613d0652979cf8bdd2c23c6e7
8-Bit Michael <mowz2012@gmail.com>
2025-10-07 16:51:26 -0700

    Updated: test_cases.py
    I added the prev_test() and next_test() functions to tinker with the new features added in playlist.py, and along the way I added some formatting to see what function was being tested, alongside making the playlist variable representing the Playlist() class accessible to all functions without it essentially refreshing.

--- test_cases.py
+++ test_cases.py
@@ -1,11 +1,14 @@
 from playlist import SongNode, Playlist
+playlist = Playlist()
 
 def test_function():
+    print('--- Test Function ---')
     song = SongNode("The Night We Met", "Lord Huron")
     result = song.playlist_display()
     print(result)
 
 def test_node():
+    print('--- Test Node ---')
     node = SongNode(
         title="Do You Remember The Times",
         artist="ISLAND",
@@ -15,13 +18,31 @@ def test_node():
     print(node.playlist_display())
 
 def test_add_song():
-    playlist = Playlist()
+    print('--- Add Song ---')
     playlist.add_song("All The Right Moves", "OneRepublic")
     playlist.add_song("Standing In The Middle Of The Field", "Haiku From Zero")
     playlist.add_song("Miol Mor Mara", "Outsider")
     playlist.add_song("Way Out There", "Lord Huron")
     playlist.display_playlist()
 
+def prev_test():
+    print('--- Previous Test ---')
+    if playlist.is_empty():
+        print("Playlist is empty.")
+    else:
+        playlist.play_previous()
+        print(playlist.show_current_song())
+
+def next_test():
+    print('--- Next Test ---')
+    if playlist.is_empty():
+        print("Playlist is empty.")
+    else:
+        playlist.play_next() # It's not returning None, but it's not going to print it
+        print(playlist.show_current_song()) # because the value has only been returned.
+
 test_function()
 test_node()
 test_add_song()
+next_test()
+prev_test()
